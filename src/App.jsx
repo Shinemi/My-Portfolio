a2beca2b70c3ee73b6e1cd52c0ee67f7e6f4a872
Shinemi <simon.lucas.83136@gmail.com>
2026-08-18 22:56:39 +0200

    fix: correct import case for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Footer from './components/footer'
-import ScrollToTop from './components/ScrollToTop'//fixed
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/home'
 import About from './pages/about'
 import Projects from './pages/projects'
